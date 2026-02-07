c5630c426948b2d2b83e069f4ba679ff08c775b5
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-02-08 01:09:17 +0300

    v1.1

--- src/test/java/org/example/reqres/tests/BaseTest.java
+++ src/test/java/org/example/reqres/tests/BaseTest.java
@@ -3,19 +3,26 @@ package org.example.reqres.tests;
 import okhttp3.OkHttpClient;
 import org.example.reqres.api.ApiKeyInterceptor;
 import org.example.reqres.api.ReqresApi;
+import org.junit.jupiter.api.BeforeEach;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BaseTest {
-    protected final OkHttpClient client = new OkHttpClient.Builder()
-            .addInterceptor(new ApiKeyInterceptor())
-            .build();
 
-    protected final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://reqres.in/")
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+    protected ReqresApi api;
 
-    protected final ReqresApi api = retrofit.create(ReqresApi.class);
+    @BeforeEach
+    void setup() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new ApiKeyInterceptor())
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://reqres.in/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        api = retrofit.create(ReqresApi.class);
+    }
 }
