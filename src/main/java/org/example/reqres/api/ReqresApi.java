824c566851c9c360eee49e56c5b46168eb73d8fc
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-02-08 00:41:43 +0300

    v1.1

--- src/main/java/org/example/reqres/api/ReqresApi.java
+++ src/main/java/org/example/reqres/api/ReqresApi.java
@@ -4,9 +4,15 @@ import org.example.reqres.models.UserRequest;
 import org.example.reqres.models.UserResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ReqresApi {
     @POST("api/register")
     Call<UserResponse> registerUser(@Body UserRequest request);
+    @GET("api/users")
+    Call<Object> getUsers(@Query("page") int page);
+    @POST("api/login")
+    Call<UserResponse> loginUser(@Body UserRequest request);
 }
