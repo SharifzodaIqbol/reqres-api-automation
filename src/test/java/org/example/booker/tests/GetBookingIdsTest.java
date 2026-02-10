5c18fbcdec0767d852e90eaa81f4ce5bec5a6bb4
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-02-10 22:51:45 +0300

    Получение списка всех ID бронирований

--- src/test/java/org/example/booker/tests/GetBookingIdsTest.java
+++ src/test/java/org/example/booker/tests/GetBookingIdsTest.java
@@ -1,12 +1,14 @@
 package org.example.booker.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BookingTest extends BaseTest {
+public class GetBookingIdsTest extends BaseTest {
 
     @Test
+    @DisplayName("Получение списка всех ID бронирований")
     public void testGetBookingIds() throws IOException {
         var response = api.getBookingIds().execute();
 
