8077a0623e8cab13539bd40311eb8a4ff174cde0
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-02-11 15:05:50 +0300

    owner

---
+++ src/test/java/org/example/booker/tests/utils/AppConfig.java
@@ -0,0 +1,47 @@
+package org.example.booker.tests.utils;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.Sources;
+
+@Sources({"classpath:message.properties"})
+public interface AppConfig extends Config {
+
+    @Key("base.url")
+    String baseUrl();
+
+    @Key("content.type")
+    String contentType();
+
+    @Key("accept.header")
+    String acceptHeader();
+
+    @Key("auth.username")
+    String username();
+
+    @Key("auth.password")
+    String password();
+
+    @Key("test.booking.firstname")
+    String bookingFirstname();
+
+    @Key("test.booking.lastname")
+    String bookingLastname();
+
+    @Key("test.booking.price")
+    int bookingPrice();
+
+    @Key("test.booking.additional")
+    String bookingAdditional();
+
+    @Key("error.network")
+    String errorNetwork();
+
+    @Key("error.auth.fail")
+    String errorAuthFail();
+
+    @Key("error.auth.token_empty")
+    String errorAuthTokenEmpty();
+
+    @Key("error.server")
+    String errorServer();
+}
